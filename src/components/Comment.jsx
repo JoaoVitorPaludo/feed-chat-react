3526630a9cb2cdcb93b670782e22d29ced868470
JoaoVitorPaludo <joaovpaludo@gmail.com>
2023-10-22 12:20:16 -0300

    feat: implementações na sessão de comentários

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/JoaoVitorPaludo.png"></img>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>João VItor Paludo</strong>
+              <time title="" dateTime="">
+                Cerca de 1 hora atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom, parabéns</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
