86bda78ffe4b52468b353929b462cf864b899bc5
JoaoVitorPaludo <joaovpaludo@gmail.com>
2023-11-27 12:30:05 -0300

    feat: implementação do contador de likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import styles from "./Comment.module.css";
 
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
   }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
+
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
@@ -19,16 +30,19 @@ export function Comment({ content, onDeleteComment }) {
                 Cerca de 1h atrás
               </time>
             </div>
-            <button title="Deletar comentário" onClick={handleDeleteComment}>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
+
           <p>{content}</p>
         </div>
+
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
